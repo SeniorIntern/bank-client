00e7563333a8759c108267499b7c14acdb760f83
SeniorIntern <tnikhil.official@gmail.com>
2024-01-17 13:07:32 +0545

    Set up Radix UI

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@radix-ui/themes";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <button>Test</button>
+      <Button>Radix Btn</Button>
+    </main>
+  )
+}
