43efa367167e8a787d1c345316ea564683e58754
SeniorIntern <tnikhil.official@gmail.com>
2024-01-19 17:06:32 +0545

    Build login and logout route

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,6 +8,11 @@ import { z } from 'zod'
 import PageContainer from '../PageContainer'
 import Image from 'next/image'
 import HumanSitting from '../HumanSitting.png'
+import { apiClient } from '../services/api-client'
+import useUserStore from '../store'
+import { useRouter } from 'next/navigation'
+import toast, { Toaster } from "react-hot-toast"
+import OnMount from '../OnMount'
 
 const loginData = z.object({
   email: z.string().email(),
@@ -17,9 +22,21 @@ const loginData = z.object({
 type FormData = z.infer<typeof loginData>
 
 const page = () => {
+  const router = useRouter()
+  const { token, setToken } = useUserStore()
+
+  if (token) router.push('/')
+
   const { register, handleSubmit, formState: { errors } } = useForm<FormData>({ resolver: zodResolver(loginData) })
-  const onSubmit = (data: FieldValues) => {
-    console.log(data);
+
+  const onSubmit = async (data: FieldValues) => {
+    apiClient.post('/auth', {
+      email: data.email,
+      password: data.password
+    }).then(data => {
+      setToken(data.data)
+      router.push('/')
+    }).catch((err) => toast.error(err.response?.data))
   }
 
   const formInputStyle: CSSProperties = {
@@ -32,41 +49,44 @@ const page = () => {
   }
 
   return (
-    <PageContainer>
-      <div className='flex'>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className='w-2/5 flex flex-col gap-8 justify-between'
-        >
-          <Text>Not registered for Online Banking?
-            <Link href={'/register'} className='text-blue-800 ml-1'>Register now</Link>
-          </Text>
-          <div>
-            <label className='block'>Email</label>
-            <input {...register('email')} placeholder='Enter your email' style={formInputStyle} />
-            <span className='text-red-600 text-[0.8rem]'>{errors.email && errors.email.message}</span>
-          </div>
+    <OnMount>
+      <PageContainer>
+        <Toaster />
+        <div className='flex'>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className='w-2/5 flex flex-col gap-8 justify-between'
+          >
+            <Text>Not registered for Online Banking?
+              <Link href={'/register'} className='text-blue-800 ml-1'>Register now</Link>
+            </Text>
+            <div>
+              <label className='block'>Email</label>
+              <input {...register('email')} placeholder='Enter your email' style={formInputStyle} />
+              <span className='text-red-600 text-[0.8rem]'>{errors.email && errors.email.message}</span>
+            </div>
 
-          <div>
-            <label className='block'>Password</label>
-            <input {...register('password')} placeholder='Enter your password' style={formInputStyle} />
-            <span className='text-red-600 text-[0.8rem]'>{errors.password && errors.password.message}</span>
-          </div>
+            <div>
+              <label className='block'>Password</label>
+              <input {...register('password')} placeholder='Enter your password' style={formInputStyle} />
+              <span className='text-red-600 text-[0.8rem]'>{errors.password && errors.password.message}</span>
+            </div>
 
-          <Button type='submit' className='w-full'>Login</Button>
-        </form>
+            <Button type='submit' className='w-full'>Login</Button>
+          </form>
 
-        <article className='w-2/5'>
-          <Image
-            src={HumanSitting}
-            alt='Human sitting'
-            width={80}
-            height={80}
-            className='h-full w-auto object-contain relative left-20'
-          />
-        </article>
-      </div>
-    </PageContainer>
+          <article className='w-2/5'>
+            <Image
+              src={HumanSitting}
+              alt='Human sitting'
+              width={80}
+              height={80}
+              className='h-full w-auto object-contain relative left-20'
+            />
+          </article>
+        </div>
+      </PageContainer>
+    </OnMount>
   )
 }
 
